aa13ec46bf994d216c4876ed9bb3f0362edf37ad
bridickens <briannadickens@u.boisestate.edu>
2024-11-12 16:56:05 -0700

    update index.js ica again

--- lambda/index.js
+++ lambda/index.js
@@ -207,7 +207,7 @@ const ErrorHandler = {
  * payloads to the handlers above. Make sure any new handlers or interceptors you've
  * defined are included below. The order matters - they're processed top to bottom 
  * */
-exports.handler = Alexa.SkillBuilders.custom()
+const skill = Alexa.SkillBuilders.custom()
     .addRequestHandlers(
         LaunchRequestHandler,
         /*HelloWorldIntentHandler,*/
@@ -223,4 +223,10 @@ exports.handler = Alexa.SkillBuilders.custom()
     .addErrorHandlers(
         ErrorHandler)
     .withCustomUserAgent('sample/hello-world/v1.2')
-    .lambda();
+    .create();
+
+    const adapter = new ExpressAdapter(skill, false, false);
+    const app = express();
+
+    app.post('/', adapter.getRequestHandlers());
+    app.listen(3010);
